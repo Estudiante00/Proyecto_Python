27b1b2de195893d16a94d9cb406de8b199925c21
Estudiante00 <studyrepositorio@gmail.com>
2024-08-26 11:17:10 -0500

    Actualizacion del proyecto

--- emociones/app/emotion_detector.py
+++ emociones/app/emotion_detector.py
@@ -1,33 +1,27 @@
-from wsgiref import headers
-import requests
+from google.cloud import language_v1
+import os
+
 class EmocionDetector:
-    def __init__(self, api_key: str, url: str):
-        self.api_key = api_key
-        self.url = url
+    def __init__(self, credentials_path: str):
+        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credentials_path
+        self.client = language_v1.LanguageServiceClient()
 
     def _validate_text(self, text: str) -> None:
         if not text.strip():
             raise ValueError("El texto no puede estar vacío")
 
     def detect_emocion(self, text: str) -> dict:
-        self._validate_text(text)#Valida que el texto no este vacio
-        #Configuracion de la solicitud a la Api de watson
-        header = {
-            'context-Type' : 'aplicacion/json',
-            'Autorizacion' : f'portador {self.api_key}'
+        self._validate_text(text)
+
+        # Preparar el documento para la API de Google Cloud
+        document = language_v1.Document(content=text, type_=language_v1.Document.Type.PLAIN_TEXT)
+
+        # Hacer la solicitud a la API
+        response = self.client.analyze_sentiment(document=document)
+        sentiment = response.document_sentiment
+
+        # Devolver el resultado en un formato adecuado
+        return {
+            "score": sentiment.score,
+            "magnitude": sentiment.magnitude
         }
-        payload = {
-            "text" : text
-        }
-        #Realiza solicitud a la API de Watson
-        response = requests.post(self.url, headers=headers, json=payload)
-        #Manejo de errores de la solicitud
-        if response.status_code !=200:
-            raise Exception(f"Error en la solicitud a waatson NLP: {response.status_code},{response.text}")
-        #Procesa y devuelve las emociones extraidas
-        return self._extract_emotions(response.json())
-    def _extract_emotions(self, data: dict) -> dict:
-        emociones = data.get('emocion',{}).get('documento',{}).get('emocion',{})
-        return emociones
-        
-        
